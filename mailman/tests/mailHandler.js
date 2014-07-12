c999c64b2be02cf998398215fe8018096c5b80db
Víctor Adán <contact@victoradan.net>
2014-07-12 13:28:31 -0400

    Added propert tests for mailHandler.js

--- mailman/tests/mailHandler.js
+++ mailman/tests/mailHandler.js
@@ -1,14 +1,37 @@
+var assert = require('assert');
 var mh = require('../handlers/mailHandler.js');
 
-var r = mh.parseMail({'subject':'FW: abc |claim id: 123'});
-console.log(r)
-var r = mh.parseMail({'subject':'claim id: 123'});
-console.log(r)
 
-var mail = {'subject':'claim id:abc'};
-var r = mh.parseMail(mail);
-console.log(r)
-var r = mh._getClaimId(mail);
-console.log(r)
+describe('mailHandler.parseMail', function() {
+    it('gets claim id with pattern \w+claim id: {id}', function(done){
+        var r = mh._getClaimId({'subject':'FW: abc |claim id: 123'});
+        assert.strictEqual(r, '123', 'claim id found in subject');
+        done();
+    });
 
-var r = mh.handleMail(mail);
+    it('gets claim id with pattern ^claim id: {id}', function(done){
+        var r = mh._getClaimId({'subject':'claim id: 123'});
+        assert.strictEqual(r, '123', 'claim id found in subject');
+        done();
+    });
+
+    it('gets claim id with pattern ^claim id:{id}', function(done){
+        var r = mh._getClaimId({'subject':'claim id:123'});
+        assert.strictEqual(r, '123', 'claim id found in subject');
+        done();
+    });
+
+    it('gets claim id with pattern ^claimid:{id}', function(done){
+        var r = mh._getClaimId({'subject':'claimid:123'});
+        assert.strictEqual(r, '123', 'claim id found in subject');
+        done();
+    });
+
+    it('gets claim id with pattern ^claimid: {id}', function(done){
+        var r = mh._getClaimId({'subject':'claimid: 123'});
+        assert.strictEqual(r, '123', 'claim id found in subject');
+        done();
+    });
+});
+
+
