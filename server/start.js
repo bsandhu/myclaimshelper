7c790a2732599ef70a8525cc21a3368fef6239f2
Baljeet Sandhu <Baljeet.mail@gmail.com>
2014-08-10 14:46:20 -0400

    - Added click on the collapsed panels. No need to click on the expand icon now.

--- server/start.js
+++ server/start.js
@@ -27,6 +27,7 @@ function setupClaimsServiceRoutes() {
     server.get('/claim', claimsService.getAllClaims);
     server.get('/claim/:id', claimsService.getClaim);
     server.get('/claim/:id/entries', claimsService.getAllEntriesForClaim);
+    server.post('/claim', claimsService.saveOrUpdateClaim);
 
     server.get('/claimEntry/:id', claimsService.getClaimEntry);
     server.post('/claimEntry', claimsService.saveOrUpdateClaimEntry);
