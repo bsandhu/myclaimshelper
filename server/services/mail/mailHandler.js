df6ab9c41f0e57b86d835ad29d9d0e42e97fa04e
Víctor Adán <vctradn@gmail.com>
2014-08-15 13:42:23 +0000

    WIP: mailman sends email reply on error.

--- server/services/mail/mailHandler.js
+++ server/services/mail/mailHandler.js
@@ -1,4 +1,6 @@
+var Mailgun = require('mailgun-js');
 var MailParser = require('./mailParser.js').MailParser;
+var config = require('../../config.js');
 
 // MailHandler
 // create new claim
@@ -15,6 +17,8 @@ MailRequestHandler.prototype.processRequest = function(req, res){
     var mailEntry = parser.parseRequest(req);
     if (parser.errors.length > 0){
         //email respond with errors
+        var body = 'ERROR: could not process request.\r\n' + parser.errors;
+        sendReply(req.params.from, req.params.subject, body)
         console.log('Errors found: ' + parser.errors);
         return false;
     }
@@ -26,5 +30,21 @@ MailRequestHandler.prototype.processRequest = function(req, res){
         return true;
     }
 };
+
+
+function sendReply(recipient, subject, body){
+    var mailgun = new Mailgun({apiKey: config.mailgun.api_key,
+                               domain: config.mailgun.domain});
+    var data = {
+        from: 'Agent 007 <no-reply@007.com>',
+        to: recipient,
+        subject: subject,
+        text: body
+    };
+    mailgun.messages().send(data, function(error, body){
+        if (error) throw error;
+        console.log(body);
+    });
+}
 
 exports.MailRequestHandler = MailRequestHandler;
