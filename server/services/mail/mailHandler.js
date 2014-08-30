d1c2291a56919f4399268c6e3b24abdb578f62a4
Víctor Adán <vctradn@gmail.com>
2014-08-30 14:48:47 +0000

    Mailman fixes:
    - ClaimEntry.description is now just the body of the email. Previously attachemnts were being stored with the ClaimEntry.
    - Attachments are now correctly stored  in the fs.files table.

--- server/services/mail/mailHandler.js
+++ server/services/mail/mailHandler.js
@@ -13,7 +13,6 @@ MailRequestHandler.prototype.processRequest = function(req, res){
     var parser = new MailParser();
     var mailEntry = parser.parseRequest(req);
     if (parser.errors.length > 0){
-        //email respond with errors
         var body = 'ERROR: could not process request.\r\n' + parser.errors;
         sendReply(req.params.from, req.params.subject, body);
         console.log('Errors found: ' + parser.errors);
@@ -23,24 +22,33 @@ MailRequestHandler.prototype.processRequest = function(req, res){
         var entry = new ClaimEntry();
         entry.entryDate = new Date();
         entry.summary = mailEntry.mail.subject;
-        entry.description = mailEntry.mail;
+        entry.description = mailEntry.mail['body-plain'];
         entry.claimId = mailEntry.claimId;
+        entry.tag = mailEntry.tags;
+        // store attachemts as such...
+        mailEntry['attachments'].forEach(function(attachment){
+            saveToDB(attachment.name, attachment.path)
+                .done(function(seqNum){
+                    console.log('Success storing attachment: ' + attachment.name);
+                    console.log('SeqNum: ' + seqNum);
+                })
+                .fail(function(err){
+                var body = 'ERROR: failed to store attachment: ';
+                body += attachment.name;
+                body += '\n' + err;
+                console.error(body);
+                sendReply(req.params.from, req.params.subject, body);
+                });
+        });
+        // store claimEntry
         claimsService.saveOrUpdateClaimEntryObject(entry)
                 .done(function (entry){
-                    //console.log(entry);
                     sendReply(req.params.from, req.params.subject, 'Success processing email!');
 		})
                 .fail(function (err){
                     console.log(err);
                     sendReply(req.params.from, req.params.subject, err);
 		});
-        // store attachemts as such...
-        for (attachment in mailEntry['attachments']){
-            saveToDB(attachment.name, attachment.path).fail(function(){
-                var body = 'ERROR: failed to store attachment: ' + attachment.name;
-                sendReply(req.params.from, req.params.subject, body);
-            });
-        }
         return true;
     }
 };
@@ -57,7 +65,7 @@ function sendReply(recipient, subject, body){
     };
     mailgun.messages().send(data, function(error, body){
         if (error) throw error;
-        else console.log(body);
+        else console.log('Mail sent successfully: ' + JSON.stringify(body));
     });
 }
 
