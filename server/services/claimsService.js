e8a43c9d42b8b9e21bf838cd09f4ca6e1edad9c9
Baljeet Sandhu <Baljeet.mail@gmail.com>
2014-09-26 18:15:33 -0400

    Fixed failing test. API broken.

--- server/services/claimsService.js
+++ server/services/claimsService.js
@@ -198,7 +198,8 @@ function getClaimEntry(req, res) {
     assert.ok(req.params.id, 'Expecting ClaimEntryId as a parameter');
     var entityId = req.params.id;
 
-    req.params.search = JSON.stringify({'_id': entityId});
+    req.body = req.body || {};
+    req.body.query = {'_id': entityId};
     searchClaimEntries(req, res);
 }
 
@@ -207,13 +208,14 @@ function getAllEntriesForClaim(req, res) {
     var claimId = req.params.id;
     console.log('Get all entries for Claim: ' + claimId);
 
-    req.params.search = JSON.stringify({'claimId': claimId});
+    req.body = req.body || {};
+    req.body.query = {'claimId': claimId};
     searchClaimEntries(req, res);
 }
 
 function searchClaimEntries(req, res) {
     var query = req.body.query;
-    var options = req.body.options;
+    var options = req.body.options || {};
 
     console.log('Searching for ClaimEntries. Req: ' + JSON.stringify(req.body));
     mongoUtils.run(function (db) {
