5a321a70d3c0d966305fbde6bc9bb0352fffa2ab
Baljeet Sandhu <Baljeet.mail@gmail.com>
2014-10-05 09:46:56 -0400

    Splitting Date and Status editors into components in prep for inline editing

---
+++ client/app/components/statusEditor/statusEditorComponent.js
@@ -0,0 +1,44 @@
+define(['knockout', 'KOMap', 'amplify', 'text!app/components/statusEditor/statusEditorComponent.tmpl.html',
+        'model/states', 'app/utils/events'],
+    function (ko, KOMap, amplify, viewHtml, States, Events) {
+        'use strict';
+
+        function StatusEditorComponentVM(params) {
+            console.log('Init Date editor widget');
+            this.States = States;
+
+            console.assert(params.statusValue, 'Expecting status observable to bind to');
+            console.assert(params.entryId, 'Expecting Entry Id');
+
+            this.state  = params.statusValue;
+            this.entryId  = params.entryId;
+            this.inEditMode = ko.observable(false);
+            this.showEditable = ko.observable(false);
+        }
+
+        StatusEditorComponentVM.prototype.onMouseOver = function (evData, ev) {
+            this.showEditable(true);
+        };
+
+        StatusEditorComponentVM.prototype.onMouseOut = function (evData, ev) {
+            this.showEditable(false);
+        };
+
+        StatusEditorComponentVM.prototype.onClick = function (evData, ev) {
+            this.inEditMode(true);
+            ev.stopImmediatePropagation();
+        };
+
+        StatusEditorComponentVM.prototype.onDismissEditor = function (evData, ev) {
+            this.inEditMode(false);
+            ev.stopImmediatePropagation();
+        };
+
+        StatusEditorComponentVM.prototype.onUpdate = function (status, entry, ev) {
+            console.log('Raise Claim Entry status update Ev. ' + this.entryId);
+            amplify.publish(Events.UPDATE_CLAIM_ENTRY_STATUS, {'claimEntryId': this.entryId, 'status': status});
+        };
+
+        return {viewModel: StatusEditorComponentVM, template: viewHtml};
+    }
+);
