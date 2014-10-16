016300d4f088bddf1b3cfb61cf6cbaf5bc7fe1dd
Mandeep Shergill <mandeeps@users.noreply.github.com>
2014-10-16 11:59:30 -0400

    autocomplete and marker

--- client/app/components/maps/mapsComponent.js
+++ client/app/components/maps/mapsComponent.js
@@ -9,18 +9,18 @@ define(['knockout', 'text!app/components/maps/mapsComponent.tmpl.html', 'async!h
             this.claimEntry = params.claimEntry;
 
             var mapOptions = {
-                center: { lat: 40.705, lng: -73.678},
+                center: {lat: 40.800, lng: -73.900},
                 zoom: 9
             };
             var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            var markers = [];
             var input = document.getElementById('pac-input');
             input.value = 'Manhattan';
             var types = document.getElementById('type-selector');
-            
+
             map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
             map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
 
-            var infowindow = new google.maps.InfoWindow();
             var autocomplete = new google.maps.places.Autocomplete(input);
             autocomplete.bindTo('bounds', map);
 
@@ -28,28 +28,42 @@ define(['knockout', 'text!app/components/maps/mapsComponent.tmpl.html', 'async!h
                 var marker = new google.maps.Marker({
                     map: map,
                     position: place.geometry.location,
-                    title: place.name,
+                    title: place.name
                 });
-                marker.setMap(map);                
-            };
+                marker.setMap(map);
+                markers.push(marker);
+            }
 
             var service = new google.maps.places.PlacesService(map);
-            service.textSearch({query: input.value}, searchCallback);
-
-            function searchCallback(results, status) {
+            service.textSearch({query: input.value}, function (results, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
                     for (var i = 0; i < results.length; i++) {
                         var place = results[i];
                         createMarker(place);
                     }
                 }
-            };
+            });
 
-            google.maps.event.addListener(input, 'place_changed', function() {
-                place = autocomplete.getPlace();
-                console.log('place is ' + place);
+            function setAllMap(map) {
+                for (var i = 0; i < markers.length; i++) {
+                    markers[i].setMap(map);
+                }
+            }
+
+            google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                var place = autocomplete.getPlace();
                 setAllMap(null);
-                service.textSearch({query: place}, searchCallback);
+                markers = [];
+                if (!place.geometry) {
+                    return;
+                }
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(17);
+                }
+                createMarker(place);
             });
 
         }
