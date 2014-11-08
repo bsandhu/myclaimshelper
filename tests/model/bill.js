0ea7fb1d92686275909d9bc69c3a83cd840fea14
Víctor Adán <contact@victoradan.net>
2014-11-08 15:31:03 -0500

    Fixed failing test in tests/model/bill.js

--- tests/model/bill.js
+++ tests/model/bill.js
@@ -2,10 +2,11 @@ var assert = require("assert");
 var Bill = require("./../../server/model/bill.js");
 
 describe('Bill', function(){
-    var bill = new Bill('abc');
+    var bill = new Bill('abc', [1,2]);
 
     it('is good', function (done) {
-      assert.equal(bill.Claim, 'abc');
+      assert.equal(bill.claimId, 'abc');
+      assert.deepEqual(bill.claimEntryIds, [1,2]);
       assert.ok(bill);
       done()
     });
