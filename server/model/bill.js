872197fa0a24963fb6c3241b464816b940806cb3
Baljeet Sandhu <Baljeet.mail@gmail.com>
2015-07-19 15:54:37 -0400

    - Persist the bill recipient.

--- server/model/bill.js
+++ server/model/bill.js
@@ -8,27 +8,33 @@ if (typeof define !== 'function') {
 define(['./billingStatus'], function (BillingStatus) {
 
     function Bill() {
-      this._id = undefined;
-      this.claimId = undefined;
-      this.billingDate = undefined;
-      this.description = undefined;
+        this._id = undefined;
+        this.claimId = undefined;
+        this.billingDate = undefined;
+        this.description = undefined;
 
-      // Amounts
-      this.preTaxTotal = undefined;
-      this.taxRate = undefined;
-      this.tax = undefined;
-      this.total = undefined;
+        // Instance of Contact.
+        // Contact details can change over time. Hence this is stored inlibe with the bill so that we can reproduce contact info exactly
+        // as it appreaded at the time of bill submission.
+        this.billRecipient = undefined;
 
-      // Category totals
-      this.totalTime = 0;
-      this.totalMileage = 0;
-      this.totalExpenseAmount = 0;
+        // Amounts
+        this.preTaxTotal = undefined;
+        this.taxRate = undefined;
+        this.tax = undefined;
+        this.total = undefined;
 
-      this.status = BillingStatus.NOT_SUBMITTED;
+        // Category totals
+        this.totalTime = 0;
+        this.totalMileage = 0;
+        this.totalExpenseAmount = 0;
 
-      // Note:
-      // BillingItems has a -> to Bill
-      // BillingService dynamically adds the BillingItems under the 'billingItems' attribute during GET Bill calls
+        this.status = BillingStatus.NOT_SUBMITTED;
+
+        // Note:
+        // BillingItems has a -> to Bill
+        // BillingService dynamically adds the BillingItems under the 'billingItems' attribute during GET Bill calls
     }
+
     return Bill;
 });
