7e506b72b55d723e6ec1f2751313f5772c3f2661
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-02-09 15:10:17 -0500

    Add type indicator to task entry

--- client/app/utils/koTaskEntryTag.js
+++ client/app/utils/koTaskEntryTag.js
@@ -3,17 +3,21 @@ define(['jquery', 'knockout', 'KOMap', 'app/utils/session', 'app/components/cont
     function ($, ko, KOMap, Session, ContactClient) {
         'use strict';
 
+        var icons = {
+                'phone' : 'fa-phone',
+                'visit' : 'fa-map-marker',
+                'photos': 'fa-camera',
+                'email' : 'fa-envelope',
+                'other' : 'fa-tag'
+        }
+
         ko.bindingHandlers.taskEntryTag = {
 
+            currentTagClass : undefined,
+
             init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-                var tag = valueAccessor();
-                var icons = {
-                    'phone' : 'fa-phone',
-                    'visit' : 'fa-map-marker',
-                    'photos': 'fa-camera',
-                    'email' : 'fa-envelope',
-                    'other' : 'fa-tag'
-                }
+                var tag = valueAccessor() || 'other';
+
                 $(element).addClass('fa');
                 $(element).addClass(icons[tag]);
                 $(element).attr('title', tag);
@@ -21,7 +25,12 @@ define(['jquery', 'knockout', 'KOMap', 'app/utils/session', 'app/components/cont
             },
 
             update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-                // No op
+                var tag = valueAccessor() || 'other';
+
+                $.each(icons, function(k, v){
+                    $(element).removeClass(v);
+                })
+                $(element).addClass(icons[tag]);
             }
         }
     }
