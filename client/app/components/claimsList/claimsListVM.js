03c0bd5418261dcad0c1b2bd847772279207de48
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-04-01 12:03:04 -0400

    Inline contact editing on claims grid

--- client/app/components/claimsList/claimsListVM.js
+++ client/app/components/claimsList/claimsListVM.js
@@ -1,7 +1,7 @@
-define(['jquery', 'knockout', 'KOMap', 'amplify', 'model/claim', 'model/claimEntry', 'app/utils/events',
+define(['jquery', 'knockout', 'KOMap', 'amplify', 'underscore', 'model/claim', 'model/claimEntry', 'app/utils/events',
         'app/utils/router', 'shared/dateUtils', 'app/utils/ajaxUtils', 'shared/objectUtils',
         'text!app/components/claimsList/claimsList.tmpl.html'],
-    function ($, ko, KOMap, amplify, Claim, ClaimEntry, Events, Router, DateUtils, AjaxUtils, ObjectUtils, claimsListView) {
+    function ($, ko, KOMap, amplify, _, Claim, ClaimEntry, Events, Router, DateUtils, AjaxUtils, ObjectUtils, claimsListView) {
         'use strict';
 
         function ClaimsListVM() {
@@ -9,7 +9,7 @@ define(['jquery', 'knockout', 'KOMap', 'amplify', 'model/claim', 'model/claimEnt
 
             // Grouping
             this.groupBy = ko.observable();
-            this.groupBy.subscribe(function(){
+            this.groupBy.subscribe(function () {
                 this.loadClaims();
             }, this);
             this.groupBy('Open');
@@ -29,8 +29,8 @@ define(['jquery', 'knockout', 'KOMap', 'amplify', 'model/claim', 'model/claimEnt
         ClaimsListVM.prototype.loadClaims = function () {
             var _this = this;
             var postReq = _this.groupBy() === 'Open'
-                            ? { query: {isClosed: false}}
-                            : { query: {isClosed: true}};
+                ? { query: {isClosed: false}}
+                : { query: {isClosed: true}};
 
             if (!_this.init) {
                 $('#claimListTable').bootstrapTable();
@@ -50,18 +50,43 @@ define(['jquery', 'knockout', 'KOMap', 'amplify', 'model/claim', 'model/claimEnt
                     $.each(data, function (index, claim) {
                         var claimDesc = claim.description || '';
                         var insuranceCoName = claim.insuranceCompanyName || '';
+                        var insuranceCoName = insuranceCoName.length > 40 ? insuranceCoName.substr(0, 40) + '...' : insuranceCoName;
+                        var insuranceCompanyFileNum = claim.insuranceCompanyFileNum || '';
+
                         var age = Math.floor((new Date().getTime() - claim.dateReceived) / (1000 * 60 * 60 * 24));
                         var ageText = age + ' days';
 
+                        var claimantName = ObjectUtils.nullSafe.bind(claim, 'this.claimantContact.name', 'None')();
+                        var claimantId = Number(ObjectUtils.nullSafe.bind(claim, 'this.claimantContact._id', '')());
+                        var claimant =
+                                _.isNumber(claimantId) && claimantId > 0
+                                    ? '<a onclick="var event = arguments[0] || window.event; ' +
+                                            'event.stopPropagation(); ' +
+                                            'amplify.publish(\'SHOW_CONTACT\', {contactId:' + claimantId + '});">' +
+                                        claimantName +
+                                    '</a>'
+                                    : claimantName;
+
+                        var insuredName = ObjectUtils.nullSafe.bind(claim, 'this.insuredContact.name', 'None')();
+                        var insuredId = Number(ObjectUtils.nullSafe.bind(claim, 'this.insuredContact._id', '')());
+                        var insured =
+                                _.isNumber(insuredId) && insuredId > 0
+                                    ? '<a onclick="var event = arguments[0] || window.event; ' +
+                                            'event.stopPropagation(); ' +
+                                            'amplify.publish(\'SHOW_CONTACT\', {contactId:' + insuredId + '});">' +
+                                        insuredName +
+                                    '</a>'
+                                    : insuredName;
+
                         tempArray.push({
                             claimId: claim._id,
-                            fileNo: claim.insuranceCompanyFileNum,
+                            fileNo: claim.fileNum,
                             desc: claimDesc.length > 40 ? claimDesc.substr(0, 40) + '...' : claimDesc,
-                            insuranceCo: insuranceCoName.length > 40 ? insuranceCoName.substr(0, 40) + '...' : insuranceCoName,
+                            insuranceCo: "<span class='text-success'>" + insuranceCompanyFileNum + "</span><br/>" + insuranceCoName,
                             dateReceived: DateUtils.niceDate(claim.dateReceived, false),
                             dateDue: DateUtils.niceDate(claim.dateDue, false),
-                            claimant: ObjectUtils.nullSafe.bind(claim, 'this.claimantContact.name', 'None')(),
-                            insured: ObjectUtils.nullSafe.bind(claim, 'this.insuredContact.name', 'None')(),
+                            claimant: claimant,
+                            insured: insured,
                             ageText: ageText,
                             age: age
                         });
