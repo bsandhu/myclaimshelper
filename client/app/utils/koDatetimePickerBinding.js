ad9e7ed76dbd8174a6607074c269ceade59312cf
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-04-09 23:16:25 -0400

    - Optional time entry while entering date

--- client/app/utils/koDatetimePickerBinding.js
+++ client/app/utils/koDatetimePickerBinding.js
@@ -10,17 +10,94 @@ define(['jquery', 'knockout', 'KOMap', 'shared/dateUtils', 'datetimepicker'],
 
             init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
                 var config = ko.utils.unwrapObservable(valueAccessor());
+                var showTime = config.timepicker;
 
+                if (!$(element).parent().is('div')){
+                    throw 'Datetime picker must have a container div as parent';
+                }
+
+                // Config for DatePicker
                 config.mask = true;
-                config.format = DateUtils.DATETIME_PICKER_FORMAT;
-
+                config.datepicker = true;
+                config.timepicker = false;
+                config.format = DateUtils.DATE_PICKER_FORMAT;
                 $(element).datetimepicker(config);
 
-                // Set the observable back as Date object
-                $(element).change(function(){
-                    console.log('Date selection: ' + $(element).val());
-                    config.dateValue(DateUtils.fromDatetimePickerFormat($(element).val()));
+                // Config for TimePicker
+                var configForTimepicker = $.extend({}, config);
+                configForTimepicker.datepicker = false;
+                configForTimepicker.timepicker = true;
+                configForTimepicker.format = DateUtils.TIME_PICKER_FORMAT;
+
+                // Modify DOM to show time picker
+                var addTimeLink = document.createElement('div');
+                $(addTimeLink).addClass('inline addTimeLink');
+                $(addTimeLink).html('<a href="#"><i class="fa fa-clock-o" style="font-size: 1.3em"></i></a>');
+                $(addTimeLink).click(function () {
+                    showTimeInput();
+                    return false;
                 });
+
+                var hideTimeLink = document.createElement('div');
+                $(hideTimeLink).html('<a href="#"><i class="fa fa-times"></i></a>');
+                $(hideTimeLink).addClass('inline leftMargin5 hideTimeLink');
+                $(hideTimeLink).click(function () {
+                    hideTimeInput();
+                    return false;
+                });
+
+                var timePickerInput = document.createElement('input');
+                $(timePickerInput).addClass('form-control narrowest input-sm timePickerInput');
+
+                var timePickerDiv = document.createElement('div');
+                $(timePickerDiv).addClass('inline timePickerDiv');
+                $(timePickerDiv).append(timePickerInput);
+                // Time picker init
+                $(timePickerInput).datetimepicker(configForTimepicker);
+                $(timePickerInput).val(DateUtils.DEFAULT_TIME_VALUE);
+
+                var containerDiv = document.createElement('div');
+                $(containerDiv).addClass('inline leftMargin5');
+                $(containerDiv).append(timePickerDiv);
+                $(containerDiv).append(hideTimeLink);
+                $(containerDiv).append(addTimeLink);
+
+                $(element).parent().append(containerDiv);
+
+                showTime
+                    ? showTimeInput()
+                    : hideTimeInput();
+
+                // Listeners
+                $(timePickerInput).change(function () {
+                    console.log('Time selection: ' + $(timePickerInput).val());
+                    updateModelObservable();
+                });
+
+                $(element).change(function () {
+                    console.log('Date selection: ' + $(element).val());
+                    updateModelObservable();
+                });
+
+                // Convert to Date obj and set on observable
+                function updateModelObservable() {
+                    var dateAsStr = $(element).val();
+                    var timeAsStr = $(timePickerInput).val();
+                    var dateTimeAsStr = dateAsStr + ' ' + timeAsStr;
+                    config.dateValue(DateUtils.fromDatetimePickerFormat(dateTimeAsStr));
+                }
+
+                function hideTimeInput(){
+                    $(addTimeLink).show();
+                    $(hideTimeLink).hide();
+                    $(timePickerDiv).hide();
+                }
+
+                function showTimeInput(){
+                    $(addTimeLink).hide();
+                    $(hideTimeLink).show();
+                    $(timePickerDiv).show();
+                }
             },
 
             update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
@@ -28,7 +105,8 @@ define(['jquery', 'knockout', 'KOMap', 'shared/dateUtils', 'datetimepicker'],
 
                 // Set the observable date as String
                 var boundDate = config.dateValue && config.dateValue() || new Date();
-                $(element).val(DateUtils.toDatetimePickerFormat(boundDate));
+                $(element).val(DateUtils.toDatePickerFormat(boundDate));
+                $(element).parent().find('.timePickerInput').val(DateUtils.toTimePickerFormat(boundDate));
             }
         };
     }
