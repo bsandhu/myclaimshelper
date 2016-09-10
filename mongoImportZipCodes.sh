911ea25fe8d3305b94a5a6346ca749fc8e0cb907
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-09-10 08:34:32 -0400

    - Add the TZ offset (bug fix)

--- mongoImportZipCodes.sh
+++ mongoImportZipCodes.sh
@@ -2,4 +2,7 @@
 # mongoimport --host localhost --port 9090 --collection ZipCodes --db AgentDb --file zipcode.json --jsonArray --upsert
 
 # Test
-mongoimport --host jello.modulusmongo.net --port 27017 --username TEST --password  PASSWORD --collection ZipCodes --db esuDub7a --file zipcode.json --jsonArray --upsert --numInsertionWorkers 2
+# mongoimport --host jello.modulusmongo.net --port 27017 --username TEST --password  PASSWORD --collection ZipCodes --db esuDub7a --file zipcode.json --jsonArray --upsert --numInsertionWorkers 2# Test
+
+# PROD
+# mongoimport --host jello.modulusmongo.net --port 27017 --username PROD --password  starWARS1 --collection ZipCodes --db u5jesaxU --file zipcode.json --jsonArray --upsert --numInsertionWorkers 2
