da6ec71dbf33bdcbc70bcefafbeabd0c32cd0436
Baljeet Sandhu <Baljeet.mail@gmail.com>
2016-09-20 17:42:11 -0400

    - Allow editing the userprofile

--- client/app/components/userProfile/userProfileComponent.js
+++ client/app/components/userProfile/userProfileComponent.js
@@ -1,15 +1,18 @@
 define(['jquery', 'knockout', 'KOMap', 'amplify',
         'app/utils/events', 'app/utils/session', 'app/utils/responsive',
         'text!app/components/userProfile/userProfile.tmpl.html', 'model/profiles',
-        'app/utils/audit', 'app/utils/consts'],
+        'app/utils/audit', 'app/utils/consts', 'app/utils/ajaxUtils'],
 
-    function ($, ko, KOMap, amplify, Events, Session, Responsive, viewHtml, UserProfile, Audit, Consts) {
+    function ($, ko, KOMap, amplify, Events, Session, Responsive, viewHtml, UserProfile,
+              Audit, Consts, AjaxUtils) {
         'use strict';
 
         function UserProfileComponent(params) {
             console.log('Init UserProfile');
             this.readyToRender = ko.observable(false);
             this.userProfile = KOMap.fromJS(new UserProfile());
+            this.editingUserProfile = ko.observable(false);
+            this.editingBillingProfile = ko.observable(false);
             this.setupEvListeners();
 
             this.login();
@@ -19,6 +22,18 @@ define(['jquery', 'knockout', 'KOMap', 'amplify',
             amplify.subscribe(Events.SHOW_USER_PROFILE, this, this.onShowUserProfile);
             amplify.subscribe(Events.SHOW_LOGIN, this, this.login);
         };
+
+        UserProfileComponent.prototype.onEditUserProfile = function (evData) {
+            this.editingUserProfile(true);
+            $('#userProfileStaticView').hide();
+            $('#userProfileEditView').fadeToggle();
+        }
+
+        UserProfileComponent.prototype.onEditBillingProfile = function (evData) {
+            this.editingBillingProfile(true);
+            $('#billingProfileStaticView').hide();
+            $('#billingProfileEditView').fadeToggle();
+        }
 
         UserProfileComponent.prototype.onShowUserProfile = function (evData) {
             console.log('UserProfileComponent - SHOW_USER_PROFILE ev ' + JSON.stringify(evData));
@@ -34,6 +49,67 @@ define(['jquery', 'knockout', 'KOMap', 'amplify',
             // Load and set in SessionStorage
             var onDoneFn = this.loadUserProfile.bind(this);
             this.checkAndSetUserAuthProfile(onDoneFn);
+        }
+
+        UserProfileComponent.prototype.onSaveBillingProfile = function () {
+            this.editingBillingProfile(false);
+            $('#billingProfileStaticView').fadeToggle();
+            $('#billingProfileEditView').hide();
+
+            var attrs = {};
+            attrs = {
+                'billingProfile.timeRate': Number(this.userProfile.billingProfile.timeRate()),
+                'billingProfile.distanceRate': Number(this.userProfile.billingProfile.distanceRate()),
+                'billingProfile.taxRate': Number(this.userProfile.billingProfile.taxRate())
+            };
+
+            AjaxUtils.post(
+                '/userProfile/modify',
+                JSON.stringify({id: Session.getCurrentUserId(), attrsAsJson: attrs}),
+                function onSuccess(response) {
+                    console.log("Saved billing profile");
+                    refreshUserProfile();
+                },
+                function onError(response) {
+                    console.error("Failed to update profile");
+                    amplify.publish(Events.FAILURE_NOTIFICATION, {msg: 'Error while saving Profile'});
+                });
+        }
+
+        UserProfileComponent.prototype.onSaveUserProfile = function () {
+            this.editingUserProfile(false);
+            $('#userProfileStaticView').fadeToggle();
+            $('#userProfileEditView').hide();
+
+            var attrs = {};
+            attrs = {
+                'contactInfo.businessName': this.userProfile.contactInfo.businessName(),
+                'contactInfo.streetAddress': this.userProfile.contactInfo.streetAddress(),
+                'contactInfo.city': this.userProfile.contactInfo.city(),
+                'contactInfo.zip': this.userProfile.contactInfo.zip(),
+                'contactInfo.phone': this.userProfile.contactInfo.phone()
+            };
+
+            AjaxUtils.post(
+                '/userProfile/modify',
+                JSON.stringify({id: Session.getCurrentUserId(), attrsAsJson: attrs}),
+                function onSuccess(response) {
+                    console.log("Saved profile");
+                    refreshUserProfile();
+                },
+                function onError(response) {
+                    console.error("Failed to update profile");
+                    amplify.publish(Events.FAILURE_NOTIFICATION, {msg: 'Error while saving Profile'});
+                });
+        }
+
+        function refreshUserProfile() {
+            $.getJSON('/userProfile/' + Session.getCurrentUserId())
+                .done(function (resp) {
+                    console.debug('Refreshed billing profile ' + JSON.stringify(resp.data).substr(0, 100));
+                    KOMap.fromJS(resp.data, {}, this.userProfile);
+                    Session.setCurrentUserProfile(resp.data);
+                })
         }
 
         /**
